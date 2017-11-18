e4a5b7f9f370a773cd1eae40140cf3773f698769
Larry Pedigo <larry.pedigo@powerhealthondemand.com>
2017-11-18 10:09:59 -0700

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('connected to server');
+
+  socket.emit('createMessage',{
+    from: "Smeep",
+    text: "I will get you squirrel!"
+  });
+});
+socket.on('disconnect',function() {
+  console.log('Disconnected from server')
+});
+
+//this is the listener for the custom call for email, which is emitted on server side.
+//data emitted from server is sent as first argument used in function. In this case "email"
+socket.on('newEmail', function(email){
+  console.log('New email',email)
+});
+//
+//createMessages
+
+socket.on('newMessage', function(message){
+  console.log('newMessage',message)
+});
--- server/server.js
+++ server/server.js
@@ -15,13 +15,25 @@ app.use(express.static(publicPath));
 //io.on is a listener to open up connection between client/server
 io.on('connect', (socket)=>{
   console.log('New user connected');
+    socket.emit('newEmail',{
+      from:'pebbles@example.com',
+      text: 'Yo yo!',
+      createAt: 123
+    });
+    socket.emit('newMessage',{
+        from: "sophie",
+        text: "behold my awesomeness",
+        createdAt: 666
+    });
+
+    socket.on('createMessage', (message)=>{
+      console.log('createMessage',message);
+    })
 
     socket.on('disconnect', ()=>{
       console.log('Client disconnected')
     });
 });
-
-
 
 server.listen(port, ()=>{
   console.log(`Server is up on port ${port}`)
